5ffbe4c351a95de99260a6380898cb38000c7c59
WillMS28 <silva.miranda.william@gmail.com>
2020-07-31 10:52:05 -0300

    Commit segundo dia de dev

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Home from './pages/home';
+import CadastroVideo from './pages/cadastro/video'
+import pagina404 from './pages/Error404'
+import CadastroCategoria from './pages/cadastro/categoria';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path = "/" component={Home} exact />
+      <Route path = "/cadastro/video" component ={CadastroVideo} exact />
+      <Route path = "/cadastro/categoria" component ={CadastroCategoria} exact />
+      <Route component = {pagina404} />
+    </Switch>  
+  </BrowserRouter>,
   document.getElementById('root')
 );
+
+
---
+++ src/pages/Error404/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function pagina404() {
+    return (
+      <div>
+        ERRO 404
+      </div>
+    )
+}
+
+export default pagina404
